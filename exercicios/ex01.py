0742665d837851550c57964c1533df8ce92898f3
Gustavo Tavares <gustavotavares785@gmail.com>
2025-11-09 23:26:08 -0300

    TESTE CI

--- exercicios/ex01.py
+++ exercicios/ex01.py
@@ -1,9 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame
 
 def ex01_create_df(spark: SparkSession) -> DataFrame:
-    """
-    Deve criar um DataFrame com colunas (id, nome) e 3 linhas.
-    """
-    # TODO: implementar
-    # Ex: return spark.createDataFrame([(1,"Alice"), (2,"Bob"), (3,"Carol")], ["id", "nome"])
+    return spark.createDataFrame([(1,"Alice"), (2,"Bob"), (3,"Carol")], ["id", "nome"])
     raise NotImplementedError
